721187d6012929d338ecef81bb063d0fc48097c2
Daniel Kim <kim506@mcmaster.ca>
2023-10-12 23:01:23 -0400

    msg

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView} from 'react-native';
 import { styles } from './styles';
 import * as Location from 'expo-location';
 import MapView, { Marker } from 'react-native-maps';
@@ -17,37 +17,56 @@ const App = () => {
 
   const mapRef = useRef(null);
 
+  const [address, setAddress] = useState(null);
+
+
 
 
   useEffect(() => {
     let locationWatcher = null;
 
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-     
-      locationWatcher = await Location.watchPositionAsync(
-        { accuracy: Location.Accuracy.High, timeInterval: 1000 , distanceInterval: 0},
-        (newLocation) => {
-          setLocation(newLocation);
-          if (mapRef.current) {
-            mapRef.current.animateToRegion({
-                latitude: newLocation.coords.latitude,
-                longitude: newLocation.coords.longitude,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1
-            })
-          }
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
         }
-      );
-      
-      
+
+        locationWatcher = await Location.watchPositionAsync(
+            { accuracy: Location.Accuracy.High, timeInterval: 1000, distanceInterval: 0 },
+            async (newLocation) => {
+                setLocation(newLocation);
+                if (mapRef.current) {
+                    mapRef.current.animateToRegion({
+                        latitude: newLocation.coords.latitude,
+                        longitude: newLocation.coords.longitude,
+                        latitudeDelta: 0.1,
+                        longitudeDelta: 0.1
+                    });
+                }
+
+                try {
+                    let results = await Location.reverseGeocodeAsync({
+                        latitude: newLocation.coords.latitude,
+                        longitude: newLocation.coords.longitude,
+                    });
+                    if (results && results.length > 0) {
+                        setAddress(results[0]);
+                    }
+                } catch (error) {
+                    console.error('Error reverse geocoding:', error);
+                }
+            }
+        );
     })();
+
+    return () => {
+        if (locationWatcher) {
+            locationWatcher.remove();
+        }
+    };
   }, []);
+
 
   let text = '';
   if (errorMsg) {
@@ -61,53 +80,71 @@ const App = () => {
     const isMocked = 'Is Mocked: ' + location.mocked;
     text += isMocked + '\n';
 
-    const altitude = 'Altitude: ' + location.coords.altitude;
+    const altitude = 'Altitude: ' + Math.round(location.coords.altitude * 1000000) / 1000000;
     text += altitude + 'm\n';
 
-    const latitude = 'Latitude: ' + location.coords.latitude;
+    const latitude = 'Latitude: ' + Math.round(location.coords.latitude * 1000000) / 1000000;
     text += latitude + '\n';
 
-    const longitude = 'Longitude: ' + location.coords.longitude;
+    const longitude = 'Longitude: ' + Math.round(location.coords.longitude * 1000000) / 1000000;
     text += longitude + '\n';
 
-    const speed = 'Speed: ' + location.coords.speed;
+    const speed = 'Speed: ' + Math.round(location.coords.speed * 1000000) / 1000000;
     text += speed + 'm/s\n';
+
+
+    let addressStreet = null;
+    let addressCity = null;
+    let addressPostal = null;
+
+    {address && (
+      addressStreet = address.streetNumber + " " + address.street,
+      addressPostal = address.postalCode,
+      addressCity = address.city + " " + address.region + " " + address.country
+    )}
+
+    text += "Address: " + addressStreet + "\nCity: " + addressCity + "\nPostal Code: " + addressPostal + "\n"
   }
 
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MoodMap</Text>
-      <Text style={styles.subtitle}>How are you feeling today?</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>MoodMap</Text>
+        <Text style={styles.subtitle}>How are you feeling today?</Text>
 
-      {moodButtons.map((moodType, index) => (
-        <Button
-          key={index}
-          title={moodType}
-          onPress={() => setMood(moodType)}
-          style={styles.moodButton}
-        />
-      ))}
-
-      <Text style={styles.result}>You're feeling {mood} today.</Text>
-      <Text style={styles.paragraph}>{text}</Text>
-
-      <MapView
-          ref={mapRef}
-          style={styles.map}
-      >
-          {location && (
-              <Marker
-                  coordinate={{
-                      latitude: location.coords.latitude,
-                      longitude: location.coords.longitude
-                  }}
-                  title="Your Location"
+        <View style={styles.buttonContainer}>  
+          {moodButtons.map((moodType, index) => (
+              <Button
+                  key={index}
+                  title={moodType}
+                  onPress={() => setMood(moodType)}
+                  style={styles.moodButton}
               />
-          )}
-      </MapView>
+          ))} 
+        </View> 
 
-    </View>
+        <Text style={styles.result}>You're feeling {mood} today.</Text>
+
+          
+        <Text style={styles.paragraph}>{text}</Text>
+
+        <MapView
+            ref={mapRef}
+            style={styles.map}
+        >
+            {location && (
+                <Marker
+                    coordinate={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude
+                    }}
+                    title="Your Location"
+                />
+            )}
+        </MapView>
+      </View>
+    </ScrollView>
     
   );
 };
--- styles.js
+++ styles.js
@@ -1,6 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+
+scrollContainer: {
+    flex: 1
+},
 container: {
     flex: 1,
     justifyContent: 'center',
@@ -11,13 +15,22 @@ title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+    marginTop: 50,
 },
 subtitle: {
     fontSize: 18,
     marginBottom: 20,
 },
+buttonContainer: {
+    flexDirection: 'row',  // Makes the children align horizontally
+    justifyContent: 'space-around',  // Distributes the children buttons with equal space around them
+    flexWrap: 'wrap',  // Allows buttons to wrap onto the next line if they run out of space
+    marginTop: 20,
+    marginBottom: 20,
+},
 moodButton: {
     marginVertical: 10,
+    marginHorizontal: 5,
 },
 result: {
     marginTop: 20,
