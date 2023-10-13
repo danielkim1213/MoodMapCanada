45009c5767165eda61035bf930a40aa33fbeff94
Daniel Kim <kim506@mcmaster.ca>
2023-10-12 21:56:19 -0400

    msg

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { styles } from './styles';
+import * as Location from 'expo-location';
 
-export default function App() {
+const App = () => {
+  const [mood, setMood] = useState('Neutral');
+
+  const moodButtons = [
+    'Happy', 'Sad', 'Calm', 'Excited', 'Angry'
+  ];
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    let locationWatcher = null;
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+     
+      locationWatcher = await Location.watchPositionAsync(
+        { accuracy: Location.Accuracy.High , timeInterval: 1000},
+        (newLocation) => {
+          setLocation(newLocation);
+          console.log(newLocation);
+        }
+      );
+      
+    })();
+  }, []);
+
+  let text = '';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+
+    const dateObject = new Date(location.timestamp);
+    const humanReadableDate = `Time: ${dateObject.getFullYear()}-${dateObject.getMonth()+1}-${dateObject.getDate()} ${dateObject.getHours()}:${dateObject.getMinutes()}:${dateObject.getSeconds()}\n`;
+    text += humanReadableDate;
+
+    const isMocked = 'Is Mocked: ' + location.mocked;
+    text += isMocked + '\n';
+
+    const altitude = 'Altitude: ' + location.coords.altitude;
+    text += altitude + 'm\n';
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <Text style={styles.title}>MoodMap</Text>
+      <Text style={styles.subtitle}>How are you feeling today?</Text>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      {moodButtons.map((moodType, index) => (
+        <Button
+          key={index}
+          title={moodType}
+          onPress={() => setMood(moodType)}
+          style={styles.moodButton}
+        />
+      ))}
+
+      <Text style={styles.result}>You're feeling {mood} today.</Text>
+      <Text style={styles.paragraph}>{text}</Text>
+
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 56.1304,    // Approximate latitude for Canada's center
+          longitude: -106.3468, // Approximate longitude for Canada's center
+          latitudeDelta: 30,    // Zoom level
+          longitudeDelta: 30,   // Zoom level
+        }}
+      />
+    </View>
+    
+  );
+};
+
+
+export default App;
---
+++ styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F7F7F7',
+},
+title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+},
+subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+},
+moodButton: {
+    marginVertical: 10,
+},
+result: {
+    marginTop: 20,
+    fontSize: 18,
+},
+map: {
+    width: '100%',
+    height: 300,
+    marginTop: 20,
+}
+});
